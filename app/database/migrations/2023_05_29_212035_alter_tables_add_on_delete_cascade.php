199141ac1da142c795361e103f1c27e4777acad1
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2023-05-29 21:44:14 -0300

    ADD migration onDelete('cascade')

---
+++ app/database/migrations/2023_05_29_212035_alter_tables_add_on_delete_cascade.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('local_impressoras', function (Blueprint $table) {
+            $table->dropForeign('local_impressoras_produto_id_foreign');
+            $table->foreign('produto_id')->references('id')->on('produtos')->onDelete('cascade');
+
+            $table->dropForeign('local_impressoras_diretoria_id_foreign');
+            $table->foreign('diretoria_id')->references('id')->on('diretorias')->onDelete('cascade');
+
+            $table->dropForeign('local_impressoras_divisao_id_foreign');
+            $table->foreign('divisao_id')->references('id')->on('divisoes')->onDelete('cascade');
+        });
+
+        Schema::table('suprimentos', function (Blueprint $table) {
+            $table->dropForeign('itens_produtos_suprimento_id_foreign');
+            $table->foreign('suprimento_id')->references('id')->on('produtos')->onDelete('cascade');
+
+            $table->dropForeign('itens_produtos_produto_id_foreign');
+            $table->foreign('produto_id')->references('id')->on('produtos')->onDelete('cascade');
+        });
+
+        Schema::table('divisoes', function (Blueprint $table) {
+            $table->dropForeign('divisoes_diretoria_id_foreign');
+            $table->foreign('diretoria_id')->references('id')->on('diretorias')->onDelete('cascade');
+        });
+
+        Schema::table('diretorias', function (Blueprint $table) {
+            $table->dropForeign('diretorias_orgao_id_foreign');
+            $table->foreign('orgao_id')->references('id')->on('orgaos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
